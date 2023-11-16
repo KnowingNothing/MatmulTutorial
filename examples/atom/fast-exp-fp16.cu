7218517abe16ad0abbd3201a4f8e913a89793f98
zhengsize <zhengsz@uw.edu>
2023-11-16 23:59:40 +0000

    fix ex2.approx usage

--- examples/atom/fast-exp-fp16.cu
+++ examples/atom/fast-exp-fp16.cu
@@ -1,40 +1,51 @@
+#include <cmath>
+
 #include "common.h"
 
-DEVICE unsigned exp2(unsigned x) {
+DEVICE unsigned nv_exp2(unsigned x) {
   unsigned ret;
   asm volatile("ex2.approx.f16x2 %0, %1;" : "=r"(ret) : "r"(x));
   return ret;
 }
 
-// thi is wrong, don't know how to fix
-DEVICE unsigned exp2(half x) {
-  half ret;
-  asm volatile("ex2.approx.f16 %0, %1;" : "=r"(ret) : "r"(x));
-  return ret;
+DEVICE half nv_exp2(half x) {
+  unsigned short v = __half_as_ushort(0.0);
+  asm volatile("ex2.approx.f16 %0, %1;" : "=h"(v) : "h"(__half_as_ushort(x)));
+  return __ushort_as_half(v);
 }
 
-__global__ void fast_exp(half* A, half* B, int N) {
+__global__ void fast_exp2(half* A, half* B, int N) {
   int x = blockIdx.x * blockDim.x + threadIdx.x;
-  auto out = reinterpret_cast<unsigned*>(B);
-  for (int i = x * 2; i * 2 < N; i += blockDim.x * gridDim.x * 2) {
-    out[i / 2] = exp2((reinterpret_cast<unsigned*>(A))[i / 2]);
+  unsigned* out = reinterpret_cast<unsigned*>(B);
+  for (int i = x * 2; i < N; i += blockDim.x * gridDim.x * 2) {
+    out[i / 2] = nv_exp2((reinterpret_cast<unsigned*>(A))[i / 2]);
   }
-  // for (int i = x; i < N; i += blockDim.x * gridDim.x) {
-  //     B[i] = exp2(A[i]);
-  // }
+  //   for (int i = x; i < N; i += blockDim.x * gridDim.x) {
+  //     B[i] = nv_exp2(A[i]);
+  //   }
+}
+
+void cpu_exp2(half* A, half* B, int N) {
+  for (int i = 0; i < N; ++i) {
+    B[i] = (half)exp2((float)A[i]);
+  }
 }
 
 int main() {
-  auto len = 100;
+  auto len = 10000;
   auto A = alloc_cpu_tensor<half>({len});
   random_fill(A, {len});
   auto B = alloc_cpu_tensor<half>({len});
+  auto golden = alloc_cpu_tensor<half>({len});
+  cpu_exp2(A, golden, len);
   auto dA = alloc_gpu_tensor<half>({len});
   auto dB = alloc_gpu_tensor<half>({len});
   copy_to_gpu(A, dA, {len});
   dim3 block(256);
   dim3 grid(ceil_div(len, block.x));
-  fast_exp<<<grid, block>>>(dA, dB, len);
+  fast_exp2<<<grid, block>>>(dA, dB, len);
   copy_to_cpu(B, dB, {len});
+  assert_allclose(B, golden, {len}, 1e-5, /*dump=*/false);
+  std::cout << "Correct!\n";
   return 0;
 }
